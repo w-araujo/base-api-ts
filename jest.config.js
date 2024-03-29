077b6264fe6307428f631f4ad095ec48d17092ba
Wesley Araujo <wesleyfaraujjo@gmail.com>
2024-03-29 20:06:33 -0300

    added jest typing

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   clearMocks: true,
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**/dist/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  moduleFileExtensions: ["js", "json"],
+  transform: {},
 };
